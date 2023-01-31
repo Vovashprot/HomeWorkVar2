57f34f2508a3634bc9102255199a1f5ce635a051
Vovashprot <vova.protasenko@gmail.com>
2023-01-31 10:24:04 +0300

    1 task

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,17 @@ public class Main {
     }
     public static void task1(){
         System.out.println("Задача 1");
-
+    byte shrek = 50;
+    int fry = 100000;
+    short sick = 20000;
+    long letMeDoItForYou = 222222222;
+    float boat = 222.22F;
+    double hehe = 2222.2222;
+    System.out.println("Значение переменной shrek с типом byte равно " + shrek);
+    System.out.println("Значение переменной fry с типом int равно " + fry);
+    System.out.println("Значение переменной sick с типом short равно " + sick);
+    System.out.println("Значение переменной letMeDoItForYou с типом long равно " + letMeDoItForYou);
+    System.out.println("Значение переменной boat с типом float равно " + boat);
+    System.out.println("Значение переменной hehe с типом double равно " + hehe);
     }
 }
