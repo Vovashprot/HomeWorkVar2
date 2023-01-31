d8f2ff01cdbc24179719fe19ff81243652b48043
Vovashprot <vova.protasenko@gmail.com>
2023-01-31 10:34:21 +0300

    1 task

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1(){
         System.out.println("Задача 1");
-    byte shrek = 50;
-    int fry = 100000;
-    short sick = 20000;
-    long letMeDoItForYou = 222222222;
-    float boat = 222.22F;
-    double hehe = 2222.2222;
-    System.out.println("Значение переменной shrek с типом byte равно " + shrek);
-    System.out.println("Значение переменной fry с типом int равно " + fry);
-    System.out.println("Значение переменной sick с типом short равно " + sick);
-    System.out.println("Значение переменной letMeDoItForYou с типом long равно " + letMeDoItForYou);
-    System.out.println("Значение переменной boat с типом float равно " + boat);
-    System.out.println("Значение переменной hehe с типом double равно " + hehe);
+        byte shrek = 50;
+        int fry = 100000;
+        short sick = 20000;
+        long letMeDoItForYou = 222222222;
+        float boat = 222.22F;
+        double hehe = 2222.2222;
+        System.out.println("Значение переменной shrek с типом byte равно " + shrek);
+        System.out.println("Значение переменной fry с типом int равно " + fry);
+        System.out.println("Значение переменной sick с типом short равно " + sick);
+        System.out.println("Значение переменной letMeDoItForYou с типом long равно " + letMeDoItForYou);
+        System.out.println("Значение переменной boat с типом float равно " + boat);
+        System.out.println("Значение переменной hehe с типом double равно " + hehe);
+    }
+    public static void task2(){
+        float banana = 27.12f;
+        long kiwi = 987678965549l;
+        int orange = 2786;
+        short leg = 569;
+        short arm = -159;
+        short torso = 27897;
+        byte apple = 67;
+
     }
 }
